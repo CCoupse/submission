bac6b8fd313576c1101bd7661ec9b1bb782ec3c9
CCoupse <juliantijulianti39@gmail.com>
2025-03-07 23:16:19 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
 
 # Set tema seaborn dan warna Streamlit
 sns.set(style='whitegrid')
